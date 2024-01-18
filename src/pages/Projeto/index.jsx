037b73236c89d95e2762da47c38e8ceef7b414de
Samuel Almeida <samuel.wow27@hotmail.com>
2024-01-18 18:21:44 -0300

    att

--- src/pages/Projeto/index.jsx
+++ src/pages/Projeto/index.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { ProjetoContainer } from "./styled";
 import { motion } from 'framer-motion'
 
 export default function Projeto() {
+
+    function getApiGitHub() {
+        fetch('https://api.github.com/users/SamuelWSGIT/repos')
+            .then(async res => {
+                if (!res.ok) {
+                    throw new Error(res.status);
+                }
+
+                let data = await res.json();
+                let listaProjetos = data.map(item => {
+                    item.name
+                })
+                return ({ listaProjetos })
+            })
+    }
+
+    getApiGitHub()
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
