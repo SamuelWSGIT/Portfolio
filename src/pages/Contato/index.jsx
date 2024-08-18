8db34ae70fde6645f7f0319f9004857d306847c1
Samuel Almeida <88410126+SamuelWSGIT@users.noreply.github.com>
2024-08-17 21:14:34 -0300

    att Contatos

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,16 +1,33 @@
+import AnimacaoColorida from "../../components/AnimacaoColorida";
+import Titulo from "../../components/Titulo";
 import { ContatoContainer } from "./styled";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 export default function Contato() {
-    return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.7 }}
-        >
-            <ContatoContainer>
-                
-            </ContatoContainer>
-        </motion.div>
-    )
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.7 }}
+    >
+      <ContatoContainer>
+        <Titulo>Contatos</Titulo>
+        <div className="contact-info">
+          <AnimacaoColorida>
+            <p>
+              E-mail:{" "}
+              <a href="mailto:mail.samuel.contato@gmail.com">
+                mail.samuel.contato@gmail.com
+              </a>
+            </p>
+          </AnimacaoColorida>
+          <AnimacaoColorida>
+            <p>
+              Telefone: <a href="tel:+5561981015267">+55 61 98101-5267</a>
+            </p>
+          </AnimacaoColorida>
+        </div>
+      </ContatoContainer>
+    </motion.div>
+  );
 }
